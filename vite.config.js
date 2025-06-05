ef88cc881becba903ba221b772bf66c9ee063e9b
Emrei28 <mreihan2804@gmail.com>
2025-06-05 14:37:04 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/',
+  base: 'https://github.com/Emrei28/react-tokoh.git',
   server: {
     port: 5173, // Pastikan port sesuai dengan yang Anda gunakan
     open: true, // Buka browser otomatis saat server berjalan
